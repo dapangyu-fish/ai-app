58e0529675cf95fbbe43f3d438ef8df3d70334c8
ci <ci@localhost>
2026-07-10 00:35:13 +0800

    fix(interpreter): import foundation for defaultTargetPlatform (fixes 58b2481 build)

--- lib/json_ui/interpreter.dart
+++ lib/json_ui/interpreter.dart
@@ -7,6 +7,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jsonlogic/jsonlogic.dart';
